c16080ade3c4b8a6fbc380a8f2015e466ec65e4d
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-01-01 19:41:29 +0100

    Use passthrough to allow other key, value

--- src/main.ts
+++ src/main.ts
@@ -2,41 +2,20 @@ import {z} from "zod";
 
 console.log('Hello world!');
 
-enum Hobbies {
-    Reading = 'reading',
-    Writing = 'writing',
-    Coding = 'coding',
-}
-
-const hobbies = ['programming', 'reading', 'sleeping'] as const;
-
 const UserSchema = z.object({
-    username: z.string().min(3).max(20),
-    email: z.string().email().optional(),
-    age: z.number().gt(0).default(Math.random() * 100),
-    salary: z.number().default(1000),
-    birthDate: z.date().nullish(),
-    isAlive: z.literal(true),
-    hobby: z.enum(hobbies),
-    hobby2: z.nativeEnum(Hobbies),
-});
+    username: z.string(),
+}).passthrough();
 
 type User = z.infer<typeof UserSchema>;
 
 const validUser = {
     username: "Gilles",
-    email: "toto@toto.to",
-    birthDate: undefined,
-    isAlive: true,
-    hobby: 'programming',
-    hobby2: Hobbies.Coding,
+    name: "Ferrand",
 };
 
 
 // Valid user
-console.log(UserSchema.shape);
-console.log(UserSchema.shape.username);
-console.log(UserSchema.partial().parse(validUser));
+console.log(UserSchema.parse(validUser));
 
 
 

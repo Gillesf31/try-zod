bf20a7826b2a836e08cd67e4f5349388802fde8d
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-01-01 13:40:28 +0100

    Use basic validation part 2

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,34 @@ import {z} from "zod";
 
 console.log('Hello world!');
 
+enum Hobbies {
+    Reading = 'reading',
+    Writing = 'writing',
+    Coding = 'coding',
+}
+
+const hobbies = ['programming', 'reading', 'sleeping'] as const;
+
 const UserSchema = z.object({
     username: z.string().min(3).max(20),
     email: z.string().email().optional(),
     age: z.number().gt(0).default(Math.random() * 100),
     salary: z.number().default(1000),
     birthDate: z.date().nullish(),
-    isAlive: z.boolean().nullable(),
+    isAlive: z.literal(true),
+    hobby: z.enum(hobbies),
+    hobby2: z.nativeEnum(Hobbies),
 });
 
 type User = z.infer<typeof UserSchema>;
 
 const validUser = {
     username: "Gilles",
-    email: "totofaitdubato@toto.to",
+    email: "toto@toto.to",
     birthDate: undefined,
-    isAlive: null,
+    isAlive: true,
+    hobby: 'programming',
+    hobby2: Hobbies.Coding,
 };
 
 

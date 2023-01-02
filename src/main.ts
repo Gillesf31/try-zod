e3831b217178b4a9b244609a31c18fbd425504ae
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-01-02 18:28:31 +0100

    Use array

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,17 @@ console.log('Hello world!');
 
 const UserSchema = z.object({
     username: z.string(),
+    friends: z.array(z.string()).nonempty(),
 }).strict();
 
 type User = z.infer<typeof UserSchema>;
 
 const validUser = {
     username: "Gilles",
-    name: "Ferrand",
+    friends: ["John", "Jane"]
 };
 
+console.log(UserSchema.shape.friends.element);
 
 // Valid user
 console.log(UserSchema.parse(validUser));

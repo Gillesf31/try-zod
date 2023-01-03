2e77306cf52f66edb065a68a601419f367563fcc
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-01-03 13:22:24 +0100

    Use discriminatedUnion

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,20 @@ import {z} from "zod";
 console.log('Hello world!');
 
 const UserSchema = z.object({
-    id: z.string().or(z.number()),
-    username: z.string(),
-    friends: z.array(z.string()).nonempty(),
-    coords: z.tuple([z.number().gt(2).int(), z.string()]),
-    coordsRest: z.tuple([z.number(), z.number()]).rest(z.number())
+    id: z.discriminatedUnion("status", [
+        z.object({ status: z.literal("success"), data: z.string() }),
+        z.object({ status: z.literal("failed"), data: z.instanceof(Error) }),
+    ])
 }).strict();
 
 type User = z.infer<typeof UserSchema>;
 
 const validUser = {
-    id: false,
-    username: "Gilles",
-    friends: ["John", "Jane"],
-    coords: [5, "Toto"],
-    coordsRest: [5, 6, 7, 8]
+    id: {
+        status: "failed",
+        data: new Error('toto')
+    },
 };
-
-console.log(UserSchema.shape.friends.element);
 
 // Valid user
 console.log(UserSchema.parse(validUser));

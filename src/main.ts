c45c79b93223f8f32376d59da4b15afc03fe9c1e
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-01-04 19:42:17 +0100

    Use Set

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import {z} from "zod";
 
 console.log('Hello world!');
 
-const UserMap = z.map(z.string(), z.number());
+const UserMap = z.set(z.number());
 
-const validUserMap = new Map([
-    ['id', 12],
-]);
+const validUserMap = new Set([1, 1, 1, 2]);
 
 // Valid user
 console.log(UserMap.parse(validUserMap));
